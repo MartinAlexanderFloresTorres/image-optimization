f7e861d2aa3d2819990bfd8543d503452eaee136
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-10-07 21:20:53 -0500

    new header

--- src/components/resultado/Resultado.jsx
+++ src/components/resultado/Resultado.jsx
@@ -2,31 +2,31 @@ import "./resultado.css";
 
 const Resultado = () => {
   return (
-    <div class='contenido_completo'>
+    <div className='contenido_completo'>
       <h3>Tu archivo convertidos</h3>
-      <div class='tabla_button'>
-        <div class='cuadro_completo'>
-          <div class='izquierda'>Cuadro vacio</div>
-          <div class='derecha'>
+      <div className='tabla_button'>
+        <div className='cuadro_completo'>
+          <div className='izquierda'>Cuadro vacio</div>
+          <div className='derecha'>
             <span>320</span>
           </div>
         </div>
       </div>
-      <div class='cuadro_3botones'>
-        <div class='Entrada_QR'>
+      <div className='cuadro_3botones'>
+        <div className='Entrada_QR'>
           <i id='qr'>QR</i>
-          <button class='btn_primary'>descargar</button>
-          <button class='btn_primary'>Cargar en la nube</button>
+          <button className='btn_primary'>descargar</button>
+          <button className='btn_primary'>Cargar en la nube</button>
         </div>
       </div>
-      <div class='cuadro_ultimoBotones'>
-        <div class='interior_icons'>
+      <div className='cuadro_ultimoBotones'>
+        <div className='interior_icons'>
           <i>Lupa</i>
           <i>QR</i>
-          <button class='btn_primary'>Convertir a otro archivo WEBP</button>
-          <button class='btn_primary'>Rehacer</button>
-          <button class='btn_primary'>Eliminar</button>
-          <button class='btn_primary'>Descargar archivo comprimido</button>
+          <button className='btn_primary'>Convertir a otro archivo WEBP</button>
+          <button className='btn_primary'>Rehacer</button>
+          <button className='btn_primary'>Eliminar</button>
+          <button className='btn_primary'>Descargar archivo comprimido</button>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Drop from "../components/drop/Drop";
+import Header from "../components/header/Header";
 import Resultado from "../components/resultado/Resultado";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Drop />
-      <Resultado />
+      {/*       <Resultado /> */}
     </>
   );
 };
