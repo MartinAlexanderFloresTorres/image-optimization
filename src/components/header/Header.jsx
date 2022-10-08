d54b2e189ff6f76ed6ae2ef6f757ec3e4c54f63d
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-10-07 23:23:46 -0500

    add new files and new features

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -352,7 +352,9 @@ const Header = () => {
             </button>
 
             <section
-              className={`header__herramientas ${herramientas ? "open" : ""}`}
+              className={`header__herramientas scroll-1 ${
+                herramientas ? "open" : ""
+              }`}
             >
               <div className='container'>
                 <ul>
@@ -740,7 +742,7 @@ const Header = () => {
           </section>
         </div>
 
-        <div className='header__right'>
+        <div className='header__right d-flex'>
           <a href='#' id='login' className='d-flex p-item'>
             <svg
               fill='none'
@@ -785,10 +787,36 @@ const Header = () => {
                     strokeLinecap='round'
                     strokeLinejoin='round'
                     strokeWidth={2}
-                    d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
+                    d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
                   />
                 </svg>
-                <span>Iniciar Sesión</span>
+                <div>
+                  <span>Nombre del usuario</span>
+                  <span className='sky'>correo@correo.com</span>
+                </div>
+              </a>
+
+              <a href='#' className='d-flex p-item item'>
+                <svg
+                  fill='none'
+                  stroke='currentColor'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z'
+                  />
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                  />
+                </svg>
+                <span>Configuración</span>
               </a>
 
               <a href='#' className='d-flex p-item item'>
@@ -808,7 +836,7 @@ const Header = () => {
                 <span>Iniciar Sesión</span>
               </a>
 
-              <a href='#' className='d-flex p-item item'>
+              <button className='d-flex p-item item'>
                 <svg
                   fill='none'
                   stroke='currentColor'
@@ -819,11 +847,11 @@ const Header = () => {
                     strokeLinecap='round'
                     strokeLinejoin='round'
                     strokeWidth={2}
-                    d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
+                    d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1'
                   />
                 </svg>
-                <span>Iniciar Sesión</span>
-              </a>
+                <span>Cerrar Sesión</span>
+              </button>
             </div>
           </div>
         </div>
